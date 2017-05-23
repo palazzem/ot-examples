f2843598bde07b5631458fd1c3c33385f5a9f541
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-05-23 11:48:06 +0200

    add ActiveSpanSource with a base noop implementation

--- proposal/tracer.py
+++ proposal/tracer.py
@@ -8,4 +8,37 @@ class Tracer(OTBaseTracer):
 
     Defined methods must be implemented as noop for the moment.
     """
-    pass
+    def __init__(self):
+        # create the tracer as defined in OpenTracing `Tracer` class
+        super(Tracer, self).__init__()
+        # this class may be changed when the vendor specific
+        # implementation is initialized
+        self._active_span_source = NoopActiveSpanSource()
+
+    @property
+    def active_span_source(self):
+        return self._active_span_source
+
+
+class BaseActiveSpanSource(object):
+    """BaseActiveSpanSource is the interface for a pluggable class that
+    defines the source for the ActiveSpan. It must be used as a base class
+    to implement the right behavior in the vendors specific implementation.
+    """
+    def make_active(self, span):
+        raise NotImplementedError
+
+    def active_span(self, span):
+        raise NotImplementedError
+
+
+class NoopActiveSpanSource(BaseActiveSpanSource):
+    """ActiveSpanSource provides the logic to get and set the current
+    ActiveSpan. This is a cheap noop implementation that must be implemented
+    by Tracer developers.
+    """
+    def make_active(self, span):
+        return None
+
+    def active_span(self):
+        return None
