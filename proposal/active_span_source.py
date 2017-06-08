70a3ff0b32fb6f8e2b542daf086d30cdba45bc30
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-06-08 09:26:54 -0400

    add some comments on the ActiveSpanSource interface

--- proposal/active_span_source.py
+++ proposal/active_span_source.py
@@ -4,12 +4,25 @@ class BaseActiveSpanSource(object):
     to implement the right behavior in the vendors specific implementation.
     """
     def make_active(self, span):
+        """Makes the given `span` active, so that it can be used as a parent
+        when calling the `start_active()` method of the Tracer. This method
+        must keep track of the active Span stack, so that the previous one
+        can be resumed when `span` is deactivated.
+        """
         raise NotImplementedError
 
-    def active_span(self, span):
+    def active_span(self):
+        """Returns the current active Span depending on the specific
+        implementation.
+        """
         raise NotImplementedError
 
     def deactivate(self, span):
+        """Deactivate the given `span`, restoring the previous active Span.
+        This method must take in consideration that a Span may be deactivated
+        when it's not really active. In that case, the current active stack
+        must not be changed.
+        """
         raise NotImplementedError
 
 
